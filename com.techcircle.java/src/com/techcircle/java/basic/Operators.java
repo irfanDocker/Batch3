1a3df60aeccda304b616d44faf872123ce931c4c
Irfan Yuksel <irfanyuksel@Irfans-MacBook-Pro.local>
2020-04-16 20:28:23 -0400

    Added operator class

---
+++ com.techcircle.java/src/com/techcircle/java/basic/Operators.java
@@ -0,0 +1,98 @@
+package com.techcircle.java.basic;
+
+public class Operators {
+	public static void main(String[] args) {
+		
+		///Addition
+		
+		byte numberA = 3;
+		
+		byte numberB = 89;
+		
+
+		System.out.println(numberA + numberB);
+		
+		
+		//subtraction
+		
+		double c = 2.4;
+		double d = 0.98;
+		
+		double result = c -d ;
+		
+		System.out.println(result);
+		
+		
+		//multiplication
+		
+		byte numberC = 10;
+		
+		
+		int resultOfMult = numberA * numberC;
+		
+		System.out.println(resultOfMult);
+		
+		
+		//Division
+		
+		int number5 = 5;
+		
+		System.out.println(resultOfMult / 2);
+		
+		
+		
+		
+		//Modular operator
+		
+//		System.out.println( numberC % 3);
+		//  10 (3 + 3 +3 +1)
+		
+		
+		System.out.println( numberC % 4);
+	//  10 (4 + 4 +2)
+		
+		
+		
+		int second = 45;
+		
+		System.out.println("line number 58");
+				
+		System.out.println(second % 2 == 0);
+
+		//java primitive data type 
+		
+		/*
+		 * 
+		 *  byte 
+		 *  short
+		 *  long
+		 *  int
+		 *  double
+		 *  float
+		 * */
+		
+		
+		
+		//Increment
+		
+		int i  = 0;
+		
+		System.out.println("From line 80 : " + i);
+		
+		i++;
+		i++;
+		i--;
+		System.out.println("From line 85 : " + i);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
