a9177aa1ac2236be4a07dc1061ff797a1158531f
Irfan Yuksel <irfanyuksel@Irfans-MacBook-Pro.local>
2020-04-16 21:03:04 -0400

    Learning operators

--- com.techcircle.java/src/com/techcircle/java/basic/Operators.java
+++ com.techcircle.java/src/com/techcircle/java/basic/Operators.java
@@ -86,6 +86,121 @@ public class Operators {
 		
 		
 		
+		//Relational Operator
+		
+		// == 
+		
+		boolean compareEquality = 3 ==3;
+		
+	
+		
+		System.out.println(compareEquality);
+		
+		
+		
+		System.out.println("Comparing 3 to 4 : " );
+		
+		System.out.println(3==4);
+		
+		//not equal !=
+		System.out.println("from line number 106 ---not equal !=");
+		System.out.println( 3 != 4);
+		
+		
+		System.out.println(  6 > 9);	//false
+		System.out.println(  6 < 9);	//true
+		System.out.println(  6 >= 9);	//false
+		System.out.println(  6 <= 9);	//true
+		
+		
+		
+		// Logical operator
+		boolean conditionA = true;
+		boolean conditionB = true;
+		boolean conditionC = false;
+		boolean conditionD = false;
+		
+		System.out.println("Logical Operator");
+		System.out.println(conditionA && conditionB); //true
+		System.out.println(conditionA && conditionC); //false
+		
+		
+		System.out.println("||");
+		System.out.println(conditionA || conditionB); // true
+		System.out.println(conditionA || conditionC); // true
+		System.out.println(conditionD || conditionC); // false
+		
+		System.out.println("Negation !");
+		
+		System.out.println(conditionA);  // true
+		System.out.println(!conditionA); // false
+		System.out.println(conditionC);
+		System.out.println(!conditionC);
+		
+		
+		
+		//
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 		
 		
